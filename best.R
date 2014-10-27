f9b717455c8823aa5682a09b8a9ab4ef23e22762
Antoine Boiron <antoine.boiron.geek@gmail.com>
2014-10-27 22:23:11 +0100

    Added examples

--- best.R
+++ best.R
@@ -33,3 +33,9 @@ best <- function(state, outcome) {
 }
 
 
+best("TX", "heart attack")
+best("TX", "heart failure")
+best("MD", "heart attack")
+best("MD", "pneumonia")
+best("BB", "heart attack")
+best("NY", "hert attack")
